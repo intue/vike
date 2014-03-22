2b4180e82dc4b623edec724c79a990dfe52ca2d8
Ruwan Nawarathne <prnawa@gmail.com>
2014-03-22 13:49:45 +0800

    V40006 Implement location awareness with http://freegeoip.net/

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,11 @@ Vike = Ember.Application.create();
 Vike.ApplicationAdapter = DS.RESTAdapter.extend({
     host: 'http://localhost:5800/api/v1'
 });
+
+function setLocation(locationData) {
+    Vike.Location = {
+        city: locationData.city,
+        country_code: locationData.country_code,
+        country_name: locationData.country_name,
+    };
+}
